9b9bdc7819412a4e5b4bd724155f0c2dfeed6262
Daniel Perkins II <daniel.perkins99@gmail.com>
2020-07-23 15:07:27 -0500

    working on calendar functionality

--- long_distance_project_front_end/src/App.js
+++ long_distance_project_front_end/src/App.js
@@ -1,39 +1,28 @@
-import React from 'react';
+import React from 'react'
+
 import beginner from './data/beginner.js'
-import intermediate from './data/intermediate.js'
-import elite from './data/elite.js'
+
 import WeekCalendarBeginner from './components/WeekCalendarBeginner.jsx';
 
 let baseURL = 'http://localhost:3003'
 
 
 class App extends React.Component {
-  state = {
-    trainingDay: []
+   state = {
+    beginner: beginner,
+    weekDays: weekDays,
+    
   }
+    
 
-  //this is supposed to retrieve the strava API info
-  getTrainingDay = () => {
-    fetch(baseURL + '/').then(res => {
-      return res.json()
-    }).then(data => {
-      console.log(data)
-      this.setState({
-        trainingDay: data,
-      })
-    })
-  }
-
-componentDidMount = () => {
-  this.getTrainingDay()
-}
 
   render () {
     return (
       <div>
         
-        <h1>Welcome to the long distance project.</h1>
-        <WeekCalendarBeginner />
+        <h1>Welcome to the long distance project.</h1> 
+        
+        <WeekCalendarBeginner beginner={this.state.beginner}/>
         
       </div>
     )
--- long_distance_project_front_end/src/components/WeekCalendarBeginner.jsx
+++ long_distance_project_front_end/src/components/WeekCalendarBeginner.jsx
@@ -1,39 +1,33 @@
-import React, {Component } from 'react'
+import React, { Component } from 'react'
+
 import beginner from '../data/beginner.js'
 
 
 class WeekCalendarBeginner extends Component {
 
-    state = ({
-        Week: '',
-        Monday: '',
-        Tuesday: '',
-        Wednesday: '',
-        Thursday: '',
-        Friday: '',
-        Saturday: '',
-        Sunday: '',
-    })
+    
 
-    checkOffDay = () => {
-        this.setState({
-            
-        })
-    }
+    // checkOffDay = () => {
+    //     this.setState({
+
+    //     })
+    // }
 
     render () {
         return (
             <div>
                 <h3>The Week Ahead:</h3>
-                <h4>Week: {beginner[0].week}</h4>
-                <div>Monday: {beginner[0].monday}</div>
-                <div>Tuesday: {beginner[0].tuesday}</div>
-                <div>Wednesday: {beginner[0].wednesday}</div>
-                <div>Thursday: {beginner[0].thursday}</div>
-                <div>Friday: {beginner[0].friday}</div>
-                <div>Saturday: {beginner[0].saturday}</div>
-                <div>Sunday: {beginner[0].sunday}</div>
-            </div>
+                {this.props.beginner.map((beginner, index) => {
+                    return <div key={index}>
+                        <p>{beginner.weekNumber}</p>
+                        </div>
+                })}
+                {this.props.beginner.map((weekDays, index) => {
+                            return <div>
+                                <p>{weekDays.day}</p>
+                            </div>
+                        })}
+                </div>
         )
     }
 }
