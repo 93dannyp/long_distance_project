3d8be29502762c18cc83520dd592b5d714ed8768
cox512 <64932698+cox512@users.noreply.github.com>
2020-07-23 21:19:44 -0500

    Got Create User working

--- long_distance_project_front_end/src/App.js
+++ long_distance_project_front_end/src/App.js
@@ -1,30 +1,37 @@
-import React from 'react'
+import React from "react";
 
-import beginner from './data/beginner.js'
+import beginner from "./data/beginner.js";
 
-import WeekCalendarBeginner from './components/WeekCalendarBeginner.jsx';
+import WeekCalendarBeginner from "./components/WeekCalendarBeginner.jsx";
+import NewUserForm from "./components/NewUserForm.jsx";
 
-let baseURL = 'http://localhost:3003'
-
+let baseURL = "http://localhost:3003";
 
 class App extends React.Component {
-   state = {
+  state = {
     beginner: beginner,
-    completedDays: [],    
-  }
-    
-  checkOffDay = (day) => {
-    this.setState({completedDays: [day, ... this.state.completedDays]})
-  }
+    completedDays: [],
+    users: [],
+  };
 
-  render () {
+  addUser = (newUser) => {
+    const copyUser = [...this.state.users];
+    copyUser.push(newUser);
+    this.setState({
+      users: copyUser,
+    });
+  };
+
+  checkOffDay = (day) => {
+    this.setState({ completedDays: [day, ...this.state.completedDays] });
+  };
+
+  render() {
     return (
       <div>
-        
-        <h1>Welcome to the long distance project.</h1> 
-        
-        <WeekCalendarBeginner beginner={this.state.beginner}/>
-        
+        <h1>Welcome to the long distance project.</h1>
+        <NewUserForm baseURL={baseURL} addUser={this.addUser} />
+        <WeekCalendarBeginner beginner={this.state.beginner} />
       </div>
     );
   }
